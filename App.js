5f9af77d801fa0f18fa27a374c1caaec63a5328b
Paul Rodgers <pcrodgers1@gmail.com>
2022-07-08 00:40:09 -0700

    setup signal r

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as signalR from '@microsoft/signalr';
+import axios from 'axios';
 
 export default function App() {
+  const apiUrlBase = 'http://localhost:7071/api';
+  const [connection, setConnection] = useState(null);
+
+  useEffect(() => {
+    const conn = new signalR.HubConnectionBuilder()
+      .withUrl(apiUrlBase)
+      .configureLogging(signalR.LogLevel.Information)
+      .build();
+
+    conn.on('createRoom', (data) => {
+      console.log(`Room ${data} created`);
+    });
+
+    conn.start().then(() => {
+      setConnection(conn);
+    });
+  }, []);
+
+  const createRoom = () => {
+    axios.post(`${apiUrlBase}/createRoom`);
+  };
+
   return (
     <View style={styles.container}>
       <Text>My first change!</Text>
+      {connection != null && (
+        <>
+          <Text>Connection Id: {connection.connectionId}</Text> <Button onPress={createRoom} title="Create Room" />
+        </>
+      )}
+
       <StatusBar style="auto" />
     </View>
   );
